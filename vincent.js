45ae643040031be8081b3a41038a0e97aa195cf8
Vincent Iadevaia <vincentiadevaia@gmail.com>
2013-09-18 08:43:14 -0700

    #latecheckin

---
+++ vincent.js
@@ -0,0 +1,12 @@
+$( window ).load( function () {
+$(".form_container").fadeIn();
+} );
+$( document ).ready( function () {
+$( ".form_container" ).hide();
+});
+$( window ).load( function () {
+$(".footer").fadeIn();
+} );
+$( document ).ready( function () {
+$( ".footer" ).hide();
+});
