ba8136599a1c578048a26c2151eef317113f18f2
Vincent Iadevaia <vincentiadevaia@gmail.com>
2013-09-23 18:06:58 -0700

    #latecheckin

--- vincent.js
+++ vincent.js
@@ -1,12 +1,5 @@
-$( window ).load( function () {
-$(".form_container").fadeIn();
-} );
-$( document ).ready( function () {
-$( ".form_container" ).hide();
-});
-$( window ).load( function () {
-$(".footer").fadeIn();
-} );
-$( document ).ready( function () {
-$( ".footer" ).hide();
-});
+$(document).ready(function(){
+        $('.hover-card').hoverCards({
+            animation: 'fade'
+        });
+    });
